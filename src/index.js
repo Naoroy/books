c4624a0892163115a2e4a77e0c6fe78a97134972
Fégan Pierre <fegan.pierre@gmail.com>
2021-10-14 16:38:44 +0200

    Testing deploy

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ db.on('error', console.error.bind(console, 'connection error:'))
 
 app.use(express.json())
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello Continuous Deployment!' })
+    res.json({ message: 'Hey Continuous Deployment!' })
   })
   .route('/book')
     .get(book.getBooks)
